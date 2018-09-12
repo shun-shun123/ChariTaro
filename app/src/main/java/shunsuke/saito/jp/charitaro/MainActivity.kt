a2409cfa63aa65fbf29354220327e71d00b92ba0
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-12 12:06:48 +0900

    :sparkles:加速度センサの変化を検出可能にした

--- app/src/main/java/shunsuke/saito/jp/charitaro/MainActivity.kt
+++ app/src/main/java/shunsuke/saito/jp/charitaro/MainActivity.kt
@@ -7,10 +7,13 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var mSensorManager: SensorManager
+    private lateinit var mAccelerometer: Sensor
+    private val SENSOR_TAG = "SENSOR_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +21,28 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         // ジャイロセンサーマネージャの取得
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        // システムにジャイロセンサーListenerを登録する
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onResume() {
+        super.onResume()
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI)
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // センサーの精度が変更された時
+        Log.d(SENSOR_TAG, "Accuracy is Changed")
+    }
+
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        if (sensorEvent?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            Log.d(SENSOR_TAG, "ACCELEROMETER sensor is changed")
+        }
     }
 }
