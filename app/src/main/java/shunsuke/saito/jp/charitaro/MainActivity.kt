5e891c70cb926622452c018a6c405b2d6e6f87e8
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-12 11:04:36 +0900

    :sparkles:SensorManagerの取得

--- app/src/main/java/shunsuke/saito/jp/charitaro/MainActivity.kt
+++ app/src/main/java/shunsuke/saito/jp/charitaro/MainActivity.kt
@@ -1,12 +1,30 @@
 package shunsuke.saito.jp.charitaro
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var mSensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ジャイロセンサーマネージャの取得
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
