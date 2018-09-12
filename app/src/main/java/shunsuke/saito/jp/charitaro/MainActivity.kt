c014703364c78d9ef0ba9edf450b10f021d8cd87
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-12 12:30:33 +0900

    :sparkles:LocationListenerインタフェース継承

--- app/src/main/java/shunsuke/saito/jp/charitaro/MainActivity.kt
+++ app/src/main/java/shunsuke/saito/jp/charitaro/MainActivity.kt
@@ -1,17 +1,20 @@
 package shunsuke.saito.jp.charitaro
 
-import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(), SensorEventListener, LocationListener {
 
     private lateinit var mSensorManager: SensorManager
+    private lateinit var mLocationManager: LocationManager
     private lateinit var mAccelerometer: Sensor
     private val SENSOR_TAG = "SENSOR_TAG"
 
@@ -20,7 +23,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         setContentView(R.layout.activity_main)
 
         // ジャイロセンサーマネージャの取得
-        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        // LocationManagerを取得
+        mLocationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         // システムにジャイロセンサーListenerを登録する
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
@@ -34,7 +39,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onPause()
         mSensorManager.unregisterListener(this)
     }
-
+    /*
+    SensorEventListenerインタフェースの実装
+     */
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         // センサーの精度が変更された時
         Log.d(SENSOR_TAG, "Accuracy is Changed")
@@ -44,5 +51,23 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         if (sensorEvent?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
             Log.d(SENSOR_TAG, "ACCELEROMETER sensor is changed")
         }
+    }
+    /*
+    LocationListenerインタフェースの実装
+     */
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
