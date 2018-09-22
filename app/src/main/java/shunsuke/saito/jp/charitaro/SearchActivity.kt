ecd3fce491763061ffd11176fb8343617dc96404
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-22 22:44:23 +0900

    :sparkles:Intentに含められた緯度経度を取得する

--- app/src/main/java/shunsuke/saito/jp/charitaro/SearchActivity.kt
+++ app/src/main/java/shunsuke/saito/jp/charitaro/SearchActivity.kt
@@ -2,11 +2,20 @@ package shunsuke.saito.jp.charitaro
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
+    private var mLongitude: Double = 0.0
+    private var mLatitude: Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        mLongitude = intent.getDoubleExtra("Longitude", 0.0)
+        mLatitude = intent.getDoubleExtra("Latitude", 0.0)
+        Log.d("SENSOR_TAG", mLongitude.toString())
+        longitude.append(mLongitude.toString())
+        latitude.append(mLatitude.toString())
     }
 }
