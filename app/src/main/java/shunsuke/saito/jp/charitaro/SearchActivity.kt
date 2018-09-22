dc11c6998bdaa6025c4fc21f9f09d4af9b321859
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-22 22:00:11 +0900

    :up:SearchActivity作成による修正

--- app/src/main/java/shunsuke/saito/jp/charitaro/SearchActivity.kt
+++ app/src/main/java/shunsuke/saito/jp/charitaro/SearchActivity.kt
@@ -1,7 +1,7 @@
 package shunsuke.saito.jp.charitaro
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
 
